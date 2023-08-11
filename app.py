5a2c58547115ef5f46f5fcfc6ceed2ffa4a9ccb0
everlyn18 <everlynjhoana@gmail.com>
2023-08-11 13:51:36 -0500

    se modifico app uno y dos

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ class Vehículo:
     def __init__(self, velocidad_maxima, kilometraje):
         self.velocidad_maxima = velocidad_maxima
         self.kilometraje = kilometraje
+
+# Crear una instancia de Vehículo
+mi_auto = Vehículo(200, 50000)
+
+# Acceder a los atributos de instancia
+print("Velocidad máxima:", mi_auto.velocidad_maxima)
+print("Kilometraje:", mi_auto.kilometraje)
